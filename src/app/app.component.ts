7e1238485ff6ce5294774543fd96a70b4fe2091f
Your Name <you@example.com>
2020-04-16 10:32:56 +0200

    add new tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { MatSliderModule } from '@angular/material/slider';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { log } from 'src/helpers';
 
 export enum TabType {
@@ -10,26 +9,48 @@ export enum TabType {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class AppComponent {
   title = 'tabs';
 
-  tabTypes = TabType;
+  TabTypes = TabType;
 
   tabsState = {
     tabs: [
       {
         tabType: TabType.CalculationDetails,
-        label: TabType.CalculationDetails
-      },
-      {
-        tabType: TabType.NewCalculation,
-        label: TabType.NewCalculation
+        label: TabType.CalculationDetails,
+        id: Date.now().valueOf()
       }
       
     ],
     selectedIndex: 0
+  }
+
+  trackByFn(index, item) {
+    return item.id; // or item.id
+  }
+
+  addNewCalculation() {
+    this.addTab(TabType.NewCalculation);
+  }
+
+  private addTab(tabType: TabType) {
+    const newTab = {tabType, label: TabType.NewCalculation, id: Date.now().valueOf()}
+    // todo, move reducing logics away to reducer
+
+    this.tabsState = {
+      ...this.tabsState,
+      tabs: [
+        ...this.tabsState.tabs,
+        newTab
+      ]
+    }
+    console.log('tab added', newTab);
+    console.log('new State', this.tabsState);
+    this.onChangeTab(this.tabsState.tabs.length);
   }
 
   onChangeTab (idx) {
